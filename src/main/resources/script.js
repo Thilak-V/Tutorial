ad5757c0f0ace2cd2827f668083210ac864723df
Thilak <thilakbhavana13@gmail.com>
2025-04-22 14:49:23 +0530

    changed.

--- src/main/resources/script.js
+++ src/main/resources/script.js
@@ -2,7 +2,7 @@ function validateLogin(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if(username === "admin" && password === "1234"){
-        window.location.href = "home.html";
+        window.location.href = "logingti.html";
         return false;
     }
     else{
