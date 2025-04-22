19d9d6a40ffa47fb5e1d4d549a6a8c725c63c8b9
Thilak <thilakbhavana13@gmail.com>
2025-04-22 14:57:24 +0530

    renamed.

--- src/main/resources/script.js
+++ src/main/resources/script.js
@@ -2,7 +2,7 @@ function validateLogin(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if(username === "admin" && password === "1234"){
-        window.location.href = "logingti.html";
+        window.location.href = "index.html";
         return false;
     }
     else{
